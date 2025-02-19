fe197b8c2af81baf56f5a74f50fce546d0e601e6
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2025-02-19 15:03:38 +0100

    added publish date as standard

--- template-parts/content/content-meta.php
+++ template-parts/content/content-meta.php
@@ -1,97 +1,63 @@
 <?php 
-    $u_time = get_the_time('U'); 
-    $u_modified_time = get_the_modified_time('U'); 
-    $has_been_updated = $u_modified_time >= $u_time + 86400;
-
     // author
     $author_id = get_the_author_meta('ID'); 
     $get_avatar_url = get_avatar_url($author_id);
-      
-    // category  
-    $isPost = false; 
-    $isReview = false; 
-    $isBonus = false; 
 
-    // Is a news post 
-    $is_news = false; // Default to false
+    // Get the category - empty if it isnt a post.
+    $categories = get_the_category(); 
 
-    if (!empty($categories)) {
-      foreach ($categories as $category) {
-        if (strpos($category->slug, 'news') !== false) { // Check if 'news' is part of the slug
-          $is_news = true;
-          break; // Exit loop once found
-        }
-      }
-    }
+    // Get the publish and updated date to compare
+    $publish_date_str = get_the_date('Y-m-d');
+    $update_date_str  = get_the_modified_date('Y-m-d');
 
-    $metaPostType = get_post_type();
-    
-    if ($metaPostType === 'review') {
-      $isReview = true; 
-    } else if ($metaPostType === 'post') {
-      $isPost = true;
-    } else if ($metaPostType === 'bonus') {
-      $isBonus = true;
-    };
+    // Get formatted dates
+    $publish_date = get_the_date('M j, Y');
+    $update_date  = get_the_modified_date('M j, Y');
 ?>
 
 
 <div class="main--meta">
-  
-  <?php if ($isPost) { ?>
-
-    <!-- Author -->
-    <div class="main--meta__section">
-      <?php if ($get_avatar_url) { ?>
-        <span class="icon">
-          <img src="<?php echo $get_avatar_url; ?>" width="20" height="20" alt="Author profile picture of <?php the_author(); ?>" /> 
-        </span>
-      <?php }; ?>
-      <span>By <?php the_author(); ?></span>
-    </div>
-    <?php }; ?>
-
-    <!-- Published -->
-    <div class="main--meta__section">
-      <span class="icon"><?php echo get_svg_icon('calendar'); ?></span>
-      <?php if ($is_news || !$has_been_updated) : ?>
-        <span><?php the_date('M jS, Y'); ?></span>
-      <?php else : ?>
-        <span>Updated <?php echo the_modified_time('M jS, Y'); ?></span>
-      <?php endif; ?>
-    </div> 
-
-    <!-- Category -->
-    <?php if (!empty($categories)) { ?>
-    <div class="main--meta__section">
-      <span class="icon"><?php echo get_svg_icon('category'); ?></span>
-      <span>
-        <?php 
-        $total_categories = count($categories);
-        $current_index = 1;
-        foreach($categories as $cat) { ?>
-          <a href="<?php echo get_category_link($cat->cat_ID); ?>"><?php echo $cat->name; ?></a>
-          <?php if ($current_index < $total_categories) { echo ','; } 
-          $current_index++; 
-        }; ?>
+  <!-- Author -->
+  <div class="main--meta__section">
+    <?php if ($get_avatar_url) { ?>
+      <span class="icon">
+        <img src="<?php echo $get_avatar_url; ?>" width="20" height="20" alt="Author profile picture of <?php the_author(); ?>" /> 
       </span>
-    </div> 
+    <?php }; ?>
+    <span>By <?php the_author(); ?></span>
+  </div>
 
-    <!-- Expiry date -->
-    <!--     
-      $expiry_date = get_field('expiry_date'); 
-      if ($expiry_date) :
+  <!-- Category -->
+  <?php if (!empty($categories)) { ?>
+  <div class="main--meta__section">
+    <span class="icon"><?php echo get_svg_icon('category'); ?></span>
 
-      <div class="main--meta__section">
-        <span class="icon"><?php echo get_svg_icon('stopwatch'); ?></span>
-        <span class="info-pill-expiry timer" data-expiry="<?php echo esc_attr($expiry_date); ?>">
-            <span class="ends-in-text"></span>
+      <?php 
+      $total_categories = count($categories);
+      $current_index = 1;
+      foreach($categories as $cat) { ?>
+        <span>
+          <a href="<?php echo get_category_link($cat->cat_ID); ?>"><?php echo $cat->name; ?></a><?php if ($current_index < $total_categories) { echo ', '; } ?>
         </span>
-      </div>
-    endif; -->
+        <?php $current_index++; ?>
+      <?php }; ?>
+    </span>  
+  </div> 
+  <?php } ?>
 
-  <?php }; ?>
+  <!-- Published --> 
+  <div class="main--meta__section">
+    <span class="icon"><?php echo get_svg_icon('calendar'); ?></span>
+    <span>Published <time datetime="<?php echo esc_attr( get_the_date('c') ); ?>"><?php echo esc_html( $publish_date ); ?></time></span>
+  </div>
 
+  <!-- Updated --> 
+  <?php if ($publish_date_str !== $update_date_str) : ?>
+  <div class="main--meta__section">
+    <span class="icon"><?php echo get_svg_icon('calendar-tick'); ?></span>
+    <span>Updated <time datetime="<?php echo esc_attr( get_the_modified_date('c') ); ?>"><?php echo esc_html( $update_date ); ?></time></span>
+  </div>
+  <?php endif; ?>
 </div>
 
 
