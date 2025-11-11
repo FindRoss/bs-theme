bd4484d7d37850b0b6e743e232962c5177cd3716
Ross Findlay <rosspfindlay@gmail.com>
2025-11-11 16:22:10 +0100

    cleaning up

--- author.php
+++ author.php
@@ -1,5 +1,7 @@
 <?php get_header();
 
+$paged              = get_query_var('paged') ? get_query_var('paged') : 1;
+
 $author             = get_queried_object();
 $author_id          = $author->ID;
 $author_name        = get_the_author_meta('display_name', $author_id);
@@ -7,28 +9,42 @@ $author_description = get_the_author_meta('description', $author_id);
 // $author_avatar      = get_avatar_url($author_id, ['size' => 120]);
 // $author_posts_url   = get_author_posts_url($author_id);
 
+$query = new WP_Query(array(
+  'post_type'  => 'post',
+  'paged'      => $paged,
+  'author'     => $author_id,
+  'meta_query' => bonus_expired_meta_query(),
+)); 
+
 ?>
 
 <div class="container">
 
   <h1><?php echo $author_name; ?></h1>
-  <div class="fs-large"><?php echo $author_description; ?></div>
-  
-  <?php if (have_posts()) : ?>
 
-    <section class="author-posts mt-5">
+  <?php if ($author_description) : ?>
+    <div class="fs-large mb-4"><?php echo $author_description; ?></div>
+  <?php endif; ?>
+  
+  <?php if ($query->have_posts()) : ?>
+
+    <section class="author-posts">
       <div class="author-posts__layout">
-        <?php while (have_posts()) : the_post(); 
+        <h2 class="h4">Articles by <?php echo $author_name; ?></h2>
+        <?php while ($query->have_posts()) : $query->the_post(); 
           get_template_part('template-parts/card/card', 'chengdu'); 
         endwhile; ?>
       </div>
-
     <?php
-      global $wp_query;
       get_template_part('template-parts/content/content', 'pagination', [
-        'query' => $wp_query
+        'query' => $query
       ]); ?>
     </section>
+
+  <?php else : ?>
+    <div>
+      <h2 class="h4">There are no articles by this author currently available.</h2>
+    </div>
   <?php endif; ?>
 
 </div>
--- template-parts/content/content-pagination.php
+++ template-parts/content/content-pagination.php
@@ -1,13 +1,13 @@
 <div class="custom-pagination py-3">
 <?php 
-$query = $args['query'];
+  $query = $args['query'];
 
-echo paginate_links( array(
-  'prev_text' => __( '<', 'textdomain' ),
-  'next_text' => __( '>', 'textdomain' ),
-  'total' => $query->max_num_pages,
-  'current' => max( 1, get_query_var('paged') ),
-  )
-);
+  echo paginate_links( array(
+    'prev_text' => __( '<', 'textdomain' ),
+    'next_text' => __( '>', 'textdomain' ),
+    'total' => $query->max_num_pages,
+    'current' => max( 1, get_query_var('paged') ),
+    )
+  );
 ?>
 </div><!-- .custom-pagination -->
