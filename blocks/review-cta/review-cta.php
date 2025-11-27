0afa7563b369fafeaf927217942d95f800b1ef6f
Ross Findlay <rosspfindlay@gmail.com>
2025-11-27 15:18:13 +0100

    restarting local

--- blocks/review-cta/review-cta.php
+++ blocks/review-cta/review-cta.php
@@ -8,7 +8,7 @@ $closed    = $details_group['closed'];
 if ($closed) return '';
 $goto_link = $details_group['affiliate_link'];
 $name      = $details_group['name'];
-// $bonus  = $details_group['bonus']; 
+$bonus     = $details_group['bonus']; 
 
 $media_group   = $id ? get_field('media_group', $id) : null;
 if (!$media_group) return '';
@@ -19,6 +19,12 @@ if ($goto_link !== null): ?>
     <a rel="nofollow" href="<?php echo esc_url($goto_link); ?>" target="_blank">
       <img src="<?php echo $featured_img; ?>" width="220" height="110" /> 
     </a>
+    <?php if ($bonus) : ?>
+      <div class="review-cta__bonus">
+        <p>Bonus</p>
+        <p><?php echo $bonus?></p>
+      </div>
+    <?php endif; ?>
     <div class="review-cta__ctas">
       <a class="button button__outline" href="<?php echo get_the_permalink($id); ?>"><?php echo $name; ?> review</a>
       <a class="button button__primary" rel="nofollow" href="<?php echo esc_url($goto_link); ?>" target="_blank">Goto <?php echo $name; ?></a>
