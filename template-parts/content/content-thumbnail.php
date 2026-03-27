86a156c84898a95d2b14e38ba79759e712d1186f
Ross Findlay <rosspfindlay@gmail.com>
2026-03-27 11:27:13 +0100

    aria label

--- template-parts/content/content-thumbnail.php
+++ template-parts/content/content-thumbnail.php
@@ -10,7 +10,7 @@
     <!-- Medium image for mobile -->
     <source media="(max-width: 767px)" srcset="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>">
     <!-- Fallback for browsers that don't support <picture> -->
-    <img class="w-100 h-auto border my-4 border-radius exclude-lazyload"
+    <img class="w-100 h-auto my-4 border-radius exclude-lazyload content-thumbnail-img"
         src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>"
         alt="<?php echo esc_attr($image_alt); ?>"
         title="<?php the_title_attribute(); ?>"
