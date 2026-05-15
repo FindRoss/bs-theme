a7e64a5ac0cc8314ee0833e901d3350848853213
Ross Findlay <rosspfindlay@gmail.com>
2026-05-15 10:10:51 +0100

    fix: taxonomy_review-type.php query returns 5 reviews instead of 6

--- taxonomy-review_type.php
+++ taxonomy-review_type.php
@@ -61,7 +61,7 @@ $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 $query = new WP_Query( 
   array( 
     'post_type'      => 'review', 
-    'posts_per_page' => 6,
+    'posts_per_page' => 5,
     'paged'          => $paged,
     'orderby'        => 'meta_value_num',
     'meta_key'       => 'rank',
