bdae49e09c06029dc0bb3d8d84b7beabd51439ea
Ross Findlay <rosspfindlay@gmail.com>
2026-06-01 13:11:44 +0100

    refactor: removed bonus from review-pill.php and added two button links

--- template-parts/card/review-pill.php
+++ template-parts/card/review-pill.php
@@ -3,8 +3,6 @@ $id          = get_the_ID();
 $details     = get_field('details_group', $id);
 $name        = $details['name'] ?? get_the_title();
 $aff_link    = $details['affiliate_link'] ?? '';
-$bonus_group = get_field('bonus_group', $id);
-$bonus_text  = $bonus_group['bonus'] ?? '';
 $review_link = get_the_permalink($id);
 $logo_url    = get_the_post_thumbnail_url($id, 'site-small-logo');
 $rank        = $args['rank'] ?? null;
@@ -15,28 +13,20 @@ $pill_class  = 'review-pill'
 ?>
 
 <div class="<?php echo esc_attr($pill_class); ?>">
-  <a class="review-pill__link" href="<?php echo esc_url($review_link); ?>" aria-label="Read <?php echo esc_attr($name); ?> review"></a>
   <?php if ($rank !== null) : ?>
     <span class="review-pill__num"><?php echo (int) $rank; ?></span>
   <?php endif; ?>
-  <span class="review-pill__logo">
+  <a class="review-pill__logo" href="<?php echo esc_url($review_link); ?>">
     <?php if ($logo_url) : ?>
       <img src="<?php echo esc_url($logo_url); ?>" alt="<?php echo esc_attr($name . ' logo'); ?>" width="96" height="44">
     <?php endif; ?>
-  </span>
-  <?php if ($bonus_text) : ?>
-  <span class="review-pill__offer">
-    <span class="review-pill__offer-icon"><?php echo get_svg_icon('present'); ?></span>
-    <span class="review-pill__offer-text"><?php echo esc_html($bonus_text); ?></span>
-  </span>
-  <?php endif; ?>
-  <span class="review-pill__visit">
+  </a>
+  <span class="review-pill__actions">
+    <a class="button button__outline" href="<?php echo esc_url($review_link); ?>">Review</a>
     <?php if ($aff_link) : ?>
-      <a href="<?php echo esc_url($aff_link); ?>" target="_blank" rel="sponsored noopener" aria-label="Visit <?php echo esc_attr($name); ?>">
-        <?php echo get_svg_icon('external-link'); ?>
-      </a>
+      <a class="button button__primary" href="<?php echo esc_url($aff_link); ?>" target="_blank" rel="sponsored noopener">Visit</a>
     <?php else : ?>
-      <span aria-hidden="true"><?php echo get_svg_icon('external-link'); ?></span>
+      <span class="button button__primary" aria-hidden="true">Visit</span>
     <?php endif; ?>
   </span>
 </div>
