3401e99d2d693ad728e71da4734c3728a70d4086
Ross Findlay <rosspfindlay@gmail.com>
2026-07-07 15:39:09 +0100

    feat: review-trust-index.php tooltip updated

--- template-parts/review/review-trust-index-details.php
+++ template-parts/review/review-trust-index-details.php
@@ -2,7 +2,7 @@
 $review_id = $args['review_id'] ?? get_the_ID();
 
 $categories = [
-  'fairness'         => ['label' => 'Fairness'],
+  'fairness'         => ['label' => 'Transparency'],
   'track_record'     => ['label' => 'Track Record'],
   'security'         => ['label' => 'Account Security'],
   'responsible'      => ['label' => 'Responsible Gambling'],
