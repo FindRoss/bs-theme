338aa9df11c8a768d898a175f3eb0cc7bf6b3069
Ross Findlay <rosspfindlay@gmail.com>
2026-09-16 15:49:39 +0100

    fix for adds

--- template-parts/ad/ad-bottom.php
+++ template-parts/ad/ad-bottom.php
@@ -1,13 +1,10 @@
-<?php if ((is_active_sidebar( 'bottom-ad' ) || is_active_sidebar('us-bottom-ad')) && !is_front_page() && !is_search()) { ?>
+<?php
+  $bottom_ad_id = (function_exists('geot_target') && geot_target('US')) ? 'us-bottom-ad' : 'bottom-ad';
+?>
+<?php if (is_active_sidebar( $bottom_ad_id ) && !is_front_page() && !is_search()) { ?>
   <aside class="advert advert__bottom">
     <div class="container">
-      <?php 
-        if (function_exists('geot_target') && geot_target( 'US' )) {
-          dynamic_sidebar('us-bottom-ad');
-        } else { 
-          dynamic_sidebar('bottom-ad');
-        };
-      ?>
+      <?php dynamic_sidebar($bottom_ad_id); ?>
     </div>
   </aside>
 <?php } ?>
--- template-parts/ad/ad-top.php
+++ template-parts/ad/ad-top.php
@@ -1,13 +1,10 @@
-<?php if ( (is_active_sidebar( 'top-ad' ) || is_active_sidebar('us-top-ad')) && !is_front_page() && !is_search()) { ?> 
+<?php
+  $top_ad_id = (function_exists('geot_target') && geot_target('US')) ? 'us-top-ad' : 'top-ad';
+?>
+<?php if ( is_active_sidebar( $top_ad_id ) && !is_front_page() && !is_search()) { ?>
   <aside class="advert advert__top">
     <div class="container">
-      <?php 
-        if (function_exists('geot_target') && geot_target( 'US' )) {
-          dynamic_sidebar('us-top-ad');
-        } else { 
-          dynamic_sidebar('top-ad');
-        };
-      ?>
+      <?php dynamic_sidebar($top_ad_id); ?>
     </div>
   </aside>
 <?php } ?>
